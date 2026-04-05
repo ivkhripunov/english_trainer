0732916ab9cf5c0a5c6e11ac023aec32e26eb727
Ivan Khripunov <khripunov.iv@phystech.edu>
2026-04-05 20:36:36 +0300

    refactor: restored files for configuration folder

--- english_trainer/wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI config for english_trainer project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'english_trainer.settings')
-application = get_wsgi_application()
